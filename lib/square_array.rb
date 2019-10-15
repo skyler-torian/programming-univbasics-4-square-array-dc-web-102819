0a272dca263e8af8766c70df4b6c5dfaee505557
Skyler Torian <skymanat7@yahoo.com>
2019-10-15 21:37:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -12,11 +12,11 @@ array[i] = array[i]**2
 puts array
 array += 1
 end
-def square_array(array)
-  new_array = []
-  array.each{|a| new_array.push(a*a)}
-  return new_array
-end
+#def square_array(array)
+ # new_array = []
+  #array.each{|a| new_array.push(a*a)}
+#  return new_array
+#end
 
 #arr = [1, 2, 3, 4, 5]
 #square_array(arr)
