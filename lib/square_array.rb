07feb4137579deb7c6ab0edf1c25e3b298b6528e
Skyler Torian <skymanat7@yahoo.com>
2019-10-15 21:41:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,22 +1,7 @@
-#def square_array(array)
-#  count = 0
-#  array = [1,2,3]
-#  while count < 2 
-#  puts count.index(0)**2
-#  count = count +1
-#end
-array = [1,2,3]
-i = 0
-while i < 3
-array[i] = array[i]**2
-puts array
-array += 1
+nums = [1,2,3]
+def square_num(nums)
+i = 0 
+while i <3
+puts nums[i]*nums[i]
+i = i + 1
 end
-#def square_array(array)
- # new_array = []
-  #array.each{|a| new_array.push(a*a)}
-#  return new_array
-#end
-
-#arr = [1, 2, 3, 4, 5]
-#square_array(arr)
